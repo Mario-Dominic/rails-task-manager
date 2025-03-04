5c6ea85ea7bf2a3ff917dd9fa924877500296a2c
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-03-04 16:53:22 +0100

    Exercise is Done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
-  def list
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task
   end
 
   def new
@@ -18,17 +20,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(tasks_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -37,5 +37,9 @@ class TasksController < ApplicationController
 
   def tasks_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
